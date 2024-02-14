7659bcce875b9377b11e79bcf674efa26ff831a0
Sly545 <sylvainferrandes@gmail.com>
2024-02-14 10:43:46 +0100

    commite de trensition qui ajoute des commentaire plus preci

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,41 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+// Importation des modules nécessaires
+const fs = require('fs'); // Module pour interagir avec le système de fichiers
+const path = require('path'); // Module pour gérer les chemins de fichiers
+const express = require('express'); // Framework pour créer des applications web
+const axios = require('axios'); // Module pour effectuer des requêtes HTTP
+const cors = require('cors'); // Middleware pour gérer les autorisations CORS
 
+// Configuration des variables d'environnement
 require('dotenv').config();
+
+// Initialisation de l'application Express
 const app = express();
 
+// Activation de CORS pour permettre les requêtes depuis http://127.0.0.1:5501
 app.use(cors({
   origin: 'http://127.0.0.1:5501',
   optionsSuccessStatus: 200
 }));
+
+// Middleware pour analyser les corps des requêtes en JSON
 app.use(express.json());
 
+// Chemin du répertoire où seront stockés les fichiers audio
 const audioDirPath = path.join(__dirname, 'audio');
+
+// Vérification et création du répertoire s'il n'existe pas déjà
 if (!fs.existsSync(audioDirPath)) {
   fs.mkdirSync(audioDirPath);
 }
 
+// Middleware pour servir les fichiers audio statiques
 app.use('/audio', express.static(audioDirPath));
-app.post('/sendMessage', async (req, res) => {
-  const { message } = req.body;
 
+// Endpoint pour envoyer un message à l'API de Mistral
+app.post('/sendMessage', async (req, res) => {
+  const { message } = req.body; // Extraction du message depuis le corps de la requête
+
+  // Vérification si le message est présent dans la requête
   if (!message) {
     return res.status(400).json({ error: 'No message provided.' });
   }
@@ -29,20 +43,20 @@ app.post('/sendMessage', async (req, res) => {
   try {
     console.log('Sending message to Mistral:', message);
     
+    // Contexte à envoyer à l'API de Mistral c''est cette ligne qui donnerat la personaliter du professeur et sa facon d'interagire avec l'utilisateur
     const lapieContext = [
       { 
         "role": "system", 
         "content": "tu est la pour que je test une api tu repondra salut ne mes aucune note de plus"
       },
-  
     ];
     
-    // Envoi de la requête à l'API de Mistral avec le contexte de Linda
+    // Envoi de la requête à l'API de Mistral
     const response = await axios.post(
       'https://api.mistral.ai/v1/chat/completions', // URL de l'API de Mistral
       {
-        model: "mistral-medium", // ou autre modèle selon votre abonnement
-        messages: lapieContext.concat([{ role: "user", content: message }]),
+        model: "mistral-medium", // Modèle utilisé pour la réponse
+        messages: lapieContext.concat([{ role: "user", content: message }]), // Contexte de la conversation
         safe_prompt: false
       },
       {
@@ -56,23 +70,25 @@ app.post('/sendMessage', async (req, res) => {
     const lapieResponse = response.data.choices[0].message.content;
     console.log('Received response from Mistral:', lapieResponse);
 
-
-
     // Envoi de la réponse de l'API après nettoyage et suppression des mots "l'utilisateur"
     res.json({ message: lapieResponse });
   } catch (error) {
+    // Gestion des erreurs
     console.error("Error caught in /sendMessage:", error.response ? error.response.data : error.message);
     res.status(500).json({ error: error.message });
   }
 });
 
+// Endpoint pour synthétiser un texte en discours audio
 app.post('/synthesizeSpeech', async (req, res) => {
-  const { text } = req.body;
+  const { text } = req.body; // Extraction du texte depuis le corps de la requête
 
+  // Vérification si le texte est présent dans la requête
   if (!text) {
     return res.status(400).json({ error: 'No text provided.' });
   }
 
+  // Options de la requête vers l'API d'Eleven Labs
   const options = {
     method: 'POST',
     headers: {
@@ -85,27 +101,39 @@ app.post('/synthesizeSpeech', async (req, res) => {
       model_id: "eleven_multilingual_v1",
     }),
     url: 'https://api.elevenlabs.io/v1/text-to-speech/Gg6guWsWupUVFCWFzqr2?optimize_streaming_latency=1&output_format=mp3_22050_32',
-    responseType: 'arraybuffer', // Important pour recevoir le flux en tant que Buffer
+    responseType: 'arraybuffer', // Définition du type de réponse attendu comme ArrayBuffer pour la requête HTTP
+    // Cela permet de traiter directement les données binaires de la réponse, utile pour les fichiers audio
   };
 
   try {
+    // Envoi de la requête à l'API d'Eleven Labs
     const response = await axios(options);
-    const audioBuffer = response.data; // C'est un Buffer contenant les données audio
+    const audioBuffer = response.data; // Contenu du fichier audio en tant que Buffer
 
-    // Générer un nom de fichier unique pour le fichier audio
+    // Génération d'un nom de fichier unique pour le fichier audio
     const audioFileName = `audio-${Date.now()}.mp3`;
     const audioFilePath = path.join(audioDirPath, audioFileName);
     
-    // Écrire le fichier audio sur le disque
+    // Écriture du fichier audio sur le disque
     fs.writeFileSync(audioFilePath, audioBuffer);
+
+    // Construction de l'URL pour accéder au fichier audio
+    const audioUrl = `${req.protocol}://${req.get('host')}/audio/${audioFileName}`;
+   
+    // Envoi de l'URL du fichier audio en réponse
+    res.json({ audioUrl: audioUrl });
+    console.log(audioUrl,audioBuffer);
     
-    // Envoyer l'URL du fichier audio au client
-    res.json({ audioUrl: `/audio/${audioFileName}` });
   } catch (error) {
+    // Gestion des erreurs
     console.error('Error synthesizing speech:', error);
     res.status(500).json({ error: 'Failed to synthesize speech' });
   }
 });
 
+// Configuration du port d'écoute du serveur
 const PORT = process.env.PORT || 5000;
+// Démarrage du serveur
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
--- script.js
+++ script.js
@@ -1,104 +1,78 @@
+// Attend que le DOM soit entièrement chargé avant d'exécuter le script
 document.addEventListener('DOMContentLoaded', function() {
-  const sendButton = document.getElementById('sendButton');
-  const messageInput = document.getElementById('messageInput');
-  const chatBox = document.getElementById('chatBox');
-  const playAudioButton = document.getElementById('playAudioButton'); // Bouton pour la synthèse vocale
-
-  sendButton.addEventListener('click', function() {
-      const messageText = messageInput.value.trim();
-      if (messageText) {
-          // Affiche le message de l'utilisateur
-          displayMessage(messageText, 'user');
-          // Envoie le message au serveur
-          sendMessageToServer(messageText);
-          // Vide le champ de saisie
-          messageInput.value = '';
-      }
-  });
-
-  playAudioButton.addEventListener('click', function() {
-    // Assuming you have a function to get the latest audio file URL
-    // This URL should be the one your backend provides after generating the audio file
-    var audioUrl = 'http://localhost:5000/audio/audio-1707224102408.mp3';
-    var audioPlayer = document.getElementById('audioPlayer');
-    audioPlayer.src = audioUrl;
-    audioPlayer.play();
-});
-
-  function sendMessageToServer(message) {
-      fetch('http://localhost:5000/sendMessage', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ message: message })
-      })
-      .then(response => response.json())
-      .then(data => {
-          displayMessage(data.message, 'bot');
-      })
-      .catch(error => {
-          console.error('Error:', error);
-          displayMessage('Une erreur est survenue.', 'bot');
-      });
-  }
-
-  function displayMessage(message, sender) {
-      const messageDiv = document.createElement('div');
-      messageDiv.textContent = message;
-      messageDiv.classList.add('message', sender);
-      chatBox.appendChild(messageDiv);
-      // Fait défiler jusqu'en bas de la zone de chat
-      chatBox.scrollTop = chatBox.scrollHeight;
-  }
-
-  function synthesizeSpeech(text) {
-    fetch('http://localhost:5000/synthesizeSpeech', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ text: text })
-    })
-    .then(response => response.json())
-    .then(data => {
-        const audioPlayer = document.getElementById('audioPlayer'); // Ensure you have an <audio id="audioPlayer"> element in your HTML
-        audioPlayer.src = data.audioUrl; // Assuming data.audioUrl is the URL to the mp3 file
-        audioPlayer.play();
-    })
-    .catch(error => {
-        console.error('Error synthesizing speech:', error);
-        alert('Une erreur est survenue lors de la synthèse vocale.');
+    // Récupération des éléments du DOM
+    const sendButton = document.getElementById('sendButton');
+    const messageInput = document.getElementById('messageInput');
+    const chatBox = document.getElementById('chatBox');
+    const playAudioButton = document.getElementById('playAudioButton');
+    const audioPlayer = document.getElementById('audioPlayer');
+    
+    // Gestion de l'événement de clic sur le bouton d'envoi de message
+    sendButton.addEventListener('click', function() {
+        const messageText = messageInput.value.trim();
+        if (messageText) {
+            displayMessage(messageText, 'user'); // Affiche le message de l'utilisateur dans la boîte de chat
+            sendMessageToServer(messageText); // Envoie le message au serveur
+            messageInput.value = ''; // Efface le champ de saisie
+        }
+        console.log(messageText);
     });
-  }
-  
 
-  // Reste du code pour l'affichage des images et la modal
+    // Envoie le message au serveur via une requête fetch
+    function sendMessageToServer(message) {
+        fetch('http://localhost:5000/sendMessage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: message }) // Envoie le message sous forme de JSON
+        })
+        .then(response => response.json())
+        .then(data => {
+            displayMessage(data.message, 'bot'); // Affiche la réponse du serveur dans la boîte de chat
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            displayMessage('Une erreur est survenue.', 'bot'); // Affiche un message d'erreur en cas de problème
+        });
+    }
+
+    // Gestion de l'événement de clic sur le bouton de lecture audio
+    playAudioButton.addEventListener('click', function() {
+        // Récupère le texte du dernier message dans la boîte de chat
+        const lastMessage = chatBox.lastElementChild ? chatBox.lastElementChild.textContent : 'Pas de message';
+        fetch('http://localhost:5000/synthesizeSpeech', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ text: "lastMessage" }) // Envoie le texte au serveur pour synthétiser le discours ps j'ai mis des guillemet pour pas que sa ùme crame mes carcther pour l'api
+        })
+        .then(response => response.json())
+        .then(data => {
+            audioPlayer.src = data.audioUrl; // Définit la source du lecteur audio avec l'URL retournée par le serveur
+            audioPlayer.play(); // Joue le fichier audio
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+
+    // Fonction pour afficher les messages dans la boîte de chat
+    function displayMessage(message, sender) {
+        const messageDiv = document.createElement('div');
+        messageDiv.textContent = message;
+        messageDiv.classList.add('message', sender); // Ajoute une classe CSS pour le style du message
+        chatBox.appendChild(messageDiv); // Ajoute le message à la boîte de chat
+        chatBox.scrollTop = chatBox.scrollHeight; // Fait défiler la boîte de chat vers le bas pour afficher le nouveau message
+    }
 });
 
-// Code pour l'affichage des images et la gestion de la modal
-function displayImage(imageUrl) {
-  const image = new Image();
-  image.src = imageUrl;
-  image.classList.add('generated-image');
-  image.onclick = function() {
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
-  }
-  chatBox.appendChild(image);
-  chatBox.scrollTop = chatBox.scrollHeight;
-}
 
-// Obtenez les éléments de la modal
-var modal = document.getElementById("myModal");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
 
-// Obtenez l'élément <span> qui ferme la modal
-var span = document.getElementsByClassName("close")[0];
 
-// Quand l'utilisateur clique sur <span> (x), fermez la modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+
+
+
+
+
